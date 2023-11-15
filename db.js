e969afd26bbb19cd520883d7b95a2e96d1d1dad0
Sinan Özdemir <60213164+SinanOzdemir55@users.noreply.github.com>
2023-11-15 14:44:25 +0100

    Create db.js
    data created

---
+++ db.js
@@ -0,0 +1,30 @@
+[
+    {
+      "id": 1,
+      "name": "John",
+      "surname": "Doe",
+      "email": "john.doe@example.com",
+      "tel": "555-1234"
+    },
+    {
+      "id": 2,
+      "name": "Jane",
+      "surname": "Smith",
+      "email": "jane.smith@example.com",
+      "tel": "555-5678"
+    },
+    {
+      "id": 3,
+      "name": "Bob",
+      "surname": "Johnson",
+      "email": "bob.johnson@example.com",
+      "tel": "555-9101"
+    }
+    {
+      "id": 4,
+      "name": "Sinan",
+      "surname": "Johnson",
+      "email": "Sinan.johnson@example.com",
+      "tel": "555-91012"
+    }
+  ]
